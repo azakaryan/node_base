1d095a707eaa4d545d2c06eac03cb4621dd957c9
azakaryan <azakaryan@azakaryan.budapest.epam.com>
2018-03-24 20:15:10 +0100

    did small mysql schema related changes

--- app/db/mysql/node_base_db_initial_data.sql
+++ app/db/mysql/node_base_db_initial_data.sql
@@ -14,7 +14,8 @@ insert into maintainers (name, surname, email) values ('Armen', 'Zakaryan', 'arm
 -- 1 Insert and Activate 1 Test User with cred -> armenzakary@gmail.com/demandvue
 -- -----------------------------------------------------
 INSERT INTO `account` (`id`, `name`, `domain_name`) VALUES (1,'test1','test1.com');
-INSERT INTO `user` (`id`, `username`, `password`, `type`, `verified`, `create_date`, `last_login`) VALUES (1000000,'armenzakary@gmail.com','$2a$10$qwm8uiGbONMXgvMzoJfJs.TsCkeDzRvFhYcnFSzsz/U03Z2yIm0fe','CLIENT',1, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `type`, `verified`, `create_date`, `last_login`) VALUES (1000000,'armenzakary@gmail.com','$2a$10$08oC45BIxg.nHBNN/9lYzudpYYSecgDZNWkj99oySvSl6.LxPJGrC','CLIENT',1, NULL, NULL);
+INSERT INTO `client` (`user_id`, `client_specific_data`) VALUES (1000000, "Some Client Specific DATA");
 INSERT INTO `account_user` (`id`, `role`, `account_id`, `user_id`) VALUES (1,'ADMIN_USER',1,1000000);
 INSERT INTO `profile` (`user_id`, `first_name`, `last_name`) VALUES (1000000, "Armen","Zakaryan");
 
@@ -22,6 +23,6 @@ INSERT INTO `profile` (`user_id`, `first_name`, `last_name`) VALUES (1000000, "A
 -- -----------------------------------------------------
 -- 3 Insert and Activate 1 Test ADMIN -> jone0089@gmail.com/demandvue
 -- -----------------------------------------------------
-INSERT INTO `user` (`id`, `username`, `password`, `type`, `verified`, `create_date`, `last_login`) VALUES (1000002,'jone0089@gmail.com','$2a$10$qwm8uiGbONMXgvMzoJfJs.TsCkeDzRvFhYcnFSzsz/U03Z2yIm0fe','ADMIN',1, NULL, NULL);
+INSERT INTO `user` (`id`, `username`, `password`, `type`, `verified`, `create_date`, `last_login`) VALUES (1000002,'jone0089@gmail.com','$2a$10$08oC45BIxg.nHBNN/9lYzudpYYSecgDZNWkj99oySvSl6.LxPJGrC','ADMIN',1, NULL, NULL);
 INSERT INTO `admin` (`user_id`, `admin_specific_data`) VALUES (1000002, "Some security DATA");
 INSERT INTO `profile` (`user_id`, `first_name`, `last_name`) VALUES (1000002, "Admin","Admin");
